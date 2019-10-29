ddcf69a5e329a20abdc8d55573c57c1aae548743
syed.Shahiq <shahiq@A002-00061.arbisoft.com>
2019-10-29 15:56:05 +0500

    Initial Commit

---
+++ src/products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class products(models.Model):
+    title = models.CharField(max_length = 120)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(decimal_places=2,max_digits=10000)
+    summary = models.TextField(default='This is summary')
