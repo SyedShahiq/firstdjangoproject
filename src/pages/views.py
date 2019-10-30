d7bfd0c531f165e0df719795a9a64abd6ae2b223
Syed Shahiq <shahiqurrehman@gmail.com>
2019-10-30 10:52:40 +0500

    views added for pages|

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*arg,**kwargs):
-	return HttpResponse('<h1>Hello World</h1>')
+def home_view(request,*arg,**kwargs):
+	return render(request,'home.html',{})
 
-def about_view(*arg,**kwargs):
-	return HttpResponse('<h1>About us Page</h1>')
+def about_view(request,*arg,**kwargs):
+	return render(request,'about.html',{})
