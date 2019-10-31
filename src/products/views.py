81b1290d535dc154a896d8d808fe0c758c048bdb
Syed Shahiq <shahiqurrehman@gmail.com>
2019-10-31 17:54:12 +0500

    Products added

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import products
 
 # Create your views here.
+def products_view_page(request):
+	product = products.objects.get(id=1)
+	context = {
+		'title':product.title,
+		'description': product.description
+	}
+	return render(request,'products/detail.html',context)
