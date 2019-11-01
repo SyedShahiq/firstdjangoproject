1168310a0520f20ec181b64f9d44dd6ebd5176f9
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-01 17:14:17 +0500

    Forms Rendering

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from .models import products
+#Django Model Form
 class productForm(forms.ModelForm):
+	title       = forms.CharField(label='Title')
+	summary     = forms.CharField(widget=forms.Textarea({
+		'placeholder':'Summarys',
+		'class': 'form-group'
+		}))
 	class Meta:
 		model = products
 		fields = [
@@ -10,6 +16,19 @@ class productForm(forms.ModelForm):
 			'summary',
 			'featured'
 		]
+	def clean_title(self,*args,**kwargs):
+		title = self.cleaned_data.get("title")
+		if "abc" in title:
+			return title
+		else:
+			raise forms.ValidationError('This is not a valid title')
 
-			
-		
+#Pure Django Form
+class productRawForm(forms.Form):
+	title       = forms.CharField(label='Title')
+	price       = forms.DecimalField()
+	summary     = forms.CharField(widget=forms.Textarea({
+		'placeholder':'Summary',
+		'class': 'abc'
+		}))
+	description = forms.CharField(required=False)
--- src/products/views.py
+++ src/products/views.py
@@ -1,29 +1,47 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import products
-from .forms import productForm
+from .forms import productForm,productRawForm
 
 # Create your views here.
+#Django Model Forms
+def products_create_view(request):
+	form = productForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+	context = {
+	'form':form
+	}
+	return render(request,'products/product_create.html',context)
+
+# Raw HTML form
+
 # def products_create_view(request):
-# 	form = productForm(request.POST or None)
-# 	if form.is_valid():
-# 		form.save()
+# 	if request.method == 'POST':
+# 		title = request.POST.get('title')
+# 		description = request.POST.get('description')
+# 		summary = request.POST.get('summary')
+# 		price = request.POST.get('price')
+# 		print(title)
+# 		products.objects.create(title=title,description=description,summary=summary,price=price)
+# 		# return http.HttpResponseRedirect('/add-products')
+# 		# products.objects.create
+# 	context = {}
+# 	return render(request,'products/product_create.html',context)
+
+#Pure Django Form
+
+# def products_create_view(request):
+# 	form = productRawForm()
+# 	if request.method == 'POST':
+# 		form = productRawForm(request.POST)
+# 		if form.is_valid():
+# 			print(form.cleaned_data)
+# 			products.objects.create(**form.cleaned_data)
 # 	context = {
-# 	'form':form
+# 		'form': form
 # 	}
 # 	return render(request,'products/product_create.html',context)
-def products_create_view(request):
-	if request.method == 'POST':
-		title = request.POST.get('title')
-		description = request.POST.get('description')
-		summary = request.POST.get('summary')
-		price = request.POST.get('price')
-		print(title)
-		products.objects.create(title=title,description=description,summary=summary,price=price)
-		# return http.HttpResponseRedirect('/add-products')
-		# products.objects.create
-	context = {}
-	return render(request,'products/product_create.html',context)
 def products_view_page(request):
 	product = products.objects.all()
 	context = {
