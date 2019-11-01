d7f14e6880178032b7b481b8bea3880baacf2161
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-01 13:55:26 +0500

    Form handling using custom forms

--- src/products/views.py
+++ src/products/views.py
@@ -4,15 +4,26 @@ from .models import products
 from .forms import productForm
 
 # Create your views here.
+# def products_create_view(request):
+# 	form = productForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 	context = {
+# 	'form':form
+# 	}
+# 	return render(request,'products/product_create.html',context)
 def products_create_view(request):
-	form = productForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-	context = {
-	'form':form
-	}
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		description = request.POST.get('description')
+		summary = request.POST.get('summary')
+		price = request.POST.get('price')
+		print(title)
+		products.objects.create(title=title,description=description,summary=summary,price=price)
+		# return http.HttpResponseRedirect('/add-products')
+		# products.objects.create
+	context = {}
 	return render(request,'products/product_create.html',context)
-
 def products_view_page(request):
 	product = products.objects.all()
 	context = {
